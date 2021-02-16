c5b8c32ddd675bca9f92af193687b234d1874547
SebCourvoisier <sebastien.courvoisier@justice.fr>
2021-02-16 14:51:16 +0100

    First commmit

---
+++ import.js
@@ -0,0 +1,373 @@
+require('dotenv').config()
+const fs = require('fs')
+const path = require('path')
+const { JurinetOracle } = require('./jurinet-oracle')
+const { JurinetUtils } = require('./jurinet-utils')
+const { JuricaOracle } = require('./jurica-oracle')
+const { JuricaUtils } = require('./jurica-utils')
+const { MongoClient } = require('mongodb')
+const crypto = require('crypto')
+const needle = require('needle')
+const batchSize = 50
+const roundSize = 5000
+const maxOffset = 5000
+const decisionsVersion = parseFloat(process.env.MONGO_DECISIONS_VERSION)
+
+console.log('Setup...')
+
+/* INIT JURINET */
+const jurinetOrder = 'DESC'
+
+const jurinetSource = new JurinetOracle({
+  verbose: true
+})
+
+const jurinetHashes = {}
+try {
+  let lines = fs.readFileSync(path.join(__dirname, 'hashes.history')).toString().split('\n')
+  lines.forEach((line) => {
+    if (line) {
+      let [id, hash] = line.split(':')
+      jurinetHashes[id] = hash
+    }
+  })
+} catch (ignore) { }
+
+let jurinetOffset = 0
+try {
+  jurinetOffset = parseInt(fs.readFileSync(path.join(__dirname, 'offset.history')).toString(), 10)
+} catch (ignore) {
+  jurinetOffset = 0
+}
+
+let jurinetEmptyRoundCount = 0
+try {
+  jurinetEmptyRoundCount = parseInt(fs.readFileSync(path.join(__dirname, 'emptyround.history')).toString(), 10)
+} catch (ignore) {
+  jurinetEmptyRoundCount = 0
+}
+if (jurinetEmptyRoundCount > roundSize || jurinetOffset > maxOffset) {
+  console.log('Jurinet - Reset loop.')
+  jurinetOffset = 0
+  jurinetEmptyRoundCount = 0
+}
+
+/* INIT JURICA */
+const juricaOrder = 'DESC'
+
+const juricaSource = new JuricaOracle({
+  verbose: true
+})
+
+const juricaHashes = {}
+try {
+  let lines = fs.readFileSync(path.join(__dirname, 'hashes_jurica.history')).toString().split('\n')
+  lines.forEach((line) => {
+    if (line) {
+      let [id, hash] = line.split(':')
+      juricaHashes[id] = hash
+    }
+  })
+} catch (ignore) { }
+
+
+let juricaOffset = 0
+try {
+  juricaOffset = parseInt(fs.readFileSync(path.join(__dirname, 'offset_jurica.history')).toString(), 10)
+} catch (ignore) {
+  juricaOffset = 0
+}
+
+let juricaEmptyRoundCount = 0
+try {
+  juricaEmptyRoundCount = parseInt(fs.readFileSync(path.join(__dirname, 'emptyround_jurica.history')).toString(), 10)
+} catch (ignore) {
+  juricaEmptyRoundCount = 0
+}
+
+if (juricaEmptyRoundCount > roundSize || juricaOffset > maxOffset) {
+  console.log('Jurica - Reset loop.')
+  juricaOffset = 0
+  juricaEmptyRoundCount = 0
+}
+
+/* MAIN LOOP */
+async function main() {
+
+  // PROCESS JURINET
+  await jurinetSource.connect()
+  const jurinetResult = await jurinetSource.getBatch({
+    offset: jurinetOffset,
+    limit: batchSize,
+    all: true,
+    titrage: true,
+    order: jurinetOrder
+  })
+  await jurinetSource.close()
+
+  if (jurinetResult) {
+    if (jurinetOrder === 'DESC') {
+      jurinetResult.sort((a, b) => {
+        if (a[process.env.MONGO_ID] < b[process.env.MONGO_ID]) {
+          return -1
+        }
+        if (a[process.env.MONGO_ID] > b[process.env.MONGO_ID]) {
+          return 1
+        }
+        return 0
+      })
+    }
+    const client = new MongoClient(process.env.MONGO_URI, {
+      useUnifiedTopology: true
+    })
+    await client.connect()
+    const database = client.db(process.env.MONGO_DBNAME)
+    const collection = database.collection(process.env.MONGO_JURINET_COLLECTION)
+    const decisions = database.collection(process.env.MONGO_DECISIONS_COLLECTION)
+    let newCount = 0
+    let updateCount = 0
+    let normalizeCount = 0
+    let errorCount = 0
+    let oldOffset = jurinetOffset
+    for (let i = 0; i < jurinetResult.length; i++) {
+      let row = jurinetResult[i]
+      let updated = false
+      const hash = crypto.createHash('md5').update(JSON.stringify(row)).digest('hex')
+      if (jurinetHashes[row[process.env.MONGO_ID]] === undefined) {
+        try {
+          await collection.insertOne(row)
+          jurinetHashes[row[process.env.MONGO_ID]] = hash
+          newCount++
+        } catch (e) {
+          console.error(e)
+          errorCount++
+        }
+      } else if (jurinetHashes[row[process.env.MONGO_ID]] !== hash) {
+        try {
+          await collection.replaceOne({ _id: row[process.env.MONGO_ID] }, row)
+          jurinetHashes[row[process.env.MONGO_ID]] = hash
+          updated = true
+          updateCount++
+        } catch (e) {
+          console.error(e)
+          errorCount++
+        }
+      }
+
+      if (row['AUT_CREATION'] !== 'WINCI' && row['TYPE_ARRET'] === 'CC') {
+        try {
+          let normalized = await decisions.findOne({ sourceId: row[process.env.MONGO_ID], sourceName: 'jurinet' })
+          if (normalized === null) {
+            let normDec = JurinetUtils.Normalize(row)
+            if (normDec.pseudoText) {
+              try {
+                normDec.zoning = await getZones(normDec.sourceId, normDec.sourceName, normDec.pseudoText)
+              } catch (e) {
+                console.error('zoning failed', normDec.sourceId, normDec.sourceName)
+                normDec.zoning = undefined
+              }
+            } else {
+              normDec.zoning = undefined
+            }
+	    normDec._version = decisionsVersion
+            await decisions.insertOne(normDec)
+            normalizeCount++
+          } else if (normalized.locked === false) {
+            if (updated === true || normalized._version !== decisionsVersion || normalized.zoning === undefined || (normalized.zoning && normalized.zoning.detail)) {
+              let normDec = JurinetUtils.Normalize(row, normalized)
+              if (normDec.pseudoText) {
+                try {
+                  normDec.zoning = await getZones(normDec.sourceId, normDec.sourceName, normDec.pseudoText)
+                } catch (e) {
+                  console.error('zoning failed', normDec.sourceId, normDec.sourceName)
+                  normDec.zoning = undefined
+                }
+              } else {
+                normDec.zoning = undefined
+              }
+	      normDec._version = decisionsVersion
+              await decisions.replaceOne({ _id: normalized[process.env.MONGO_ID] }, normDec)
+              normalizeCount++
+            }
+          }
+        } catch (e) {
+          console.error(e)
+          errorCount++
+        }
+      } else {
+        try {
+          let normalized = await decisions.findOne({ sourceId: row[process.env.MONGO_ID], sourceName: 'jurinet' })
+          if (normalized !== null) {
+            await decisions.deleteOne({ _id: normalized[process.env.MONGO_ID] })
+          }
+        } catch (e) {
+          console.error(e)
+          errorCount++
+        }
+      }
+
+      jurinetOffset++
+    }
+    console.log(`Jurinet - (${oldOffset}-${(oldOffset + batchSize)}) New: ${newCount}, Update: ${updateCount}, Normalize: ${normalizeCount}, Error: ${errorCount}.`)
+    if (newCount === 0 && updateCount === 0 && normalizeCount === 0) {
+      jurinetEmptyRoundCount++
+    } else {
+      jurinetEmptyRoundCount = 0
+    }
+    await client.close()
+  } else {
+    jurinetOffset = 0
+    jurinetEmptyRoundCount = 0
+  }
+
+  // PROCESS JURICA
+  await juricaSource.connect()
+  const juricaResult = await juricaSource.getBatch({
+    offset: juricaOffset,
+    limit: batchSize,
+    all: true,
+    titrage: false,
+    order: juricaOrder
+  })
+  await juricaSource.close()
+
+  if (juricaResult) {
+    if (juricaOrder === 'DESC') {
+      juricaResult.sort((a, b) => {
+        if (a[process.env.MONGO_ID] < b[process.env.MONGO_ID]) {
+          return -1
+        }
+        if (a[process.env.MONGO_ID] > b[process.env.MONGO_ID]) {
+          return 1
+        }
+        return 0
+      })
+    }
+    const client = new MongoClient(process.env.MONGO_URI, {
+      useUnifiedTopology: true
+    })
+    await client.connect()
+    const database = client.db(process.env.MONGO_DBNAME)
+    const collection = database.collection(process.env.MONGO_JURICA_COLLECTION)
+    const decisions = database.collection(process.env.MONGO_DECISIONS_COLLECTION)
+    let newCount = 0
+    let updateCount = 0
+    let normalizeCount = 0
+    let errorCount = 0
+    let oldOffset = juricaOffset
+    for (let i = 0; i < juricaResult.length; i++) {
+      let row = juricaResult[i]
+      let updated = false
+      const hash = crypto.createHash('md5').update(JSON.stringify(row)).digest('hex')
+      if (juricaHashes[row[process.env.MONGO_ID]] === undefined) {
+        try {
+          await collection.insertOne(row)
+          juricaHashes[row[process.env.MONGO_ID]] = hash
+          newCount++
+        } catch (e) {
+          console.error(e)
+          errorCount++
+        }
+      } else if (juricaHashes[row[process.env.MONGO_ID]] !== hash) {
+        try {
+          await collection.replaceOne({ _id: row[process.env.MONGO_ID] }, row)
+          juricaHashes[row[process.env.MONGO_ID]] = hash
+          updated = true
+          updateCount++
+        } catch (e) {
+          console.error(e)
+          errorCount++
+        }
+      }
+
+      try {
+        let normalized = await decisions.findOne({ sourceId: row[process.env.MONGO_ID], sourceName: 'jurica' })
+        if (normalized === null) {
+          let normDec = JuricaUtils.Normalize(row)
+          if (normDec.pseudoText) {
+            try {
+              normDec.zoning = await getZones(normDec.sourceId, normDec.sourceName, normDec.pseudoText)
+            } catch (e) {
+              console.error('zoning failed', normDec.sourceId, normDec.sourceName)
+              normDec.zoning = undefined
+            }
+          } else {
+            normDec.zoning = undefined
+          }
+	  normDec._version = decisionsVersion
+          await decisions.insertOne(normDec)
+          normalizeCount++
+        } else if (normalized.locked === false) {
+          if (updated === true || normalized._version !== decisionsVersion || normalized.zoning === undefined || (normalized.zoning && normalized.zoning.detail)) {
+            let normDec = JuricaUtils.Normalize(row, normalized)
+            if (normDec.pseudoText) {
+              try {
+                normDec.zoning = await getZones(normDec.sourceId, normDec.sourceName, normDec.pseudoText)
+              } catch (e) {
+                console.error('zoning failed', normDec.sourceId, normDec.sourceName)
+                normDec.zoning = undefined
+              }
+            } else {
+              normDec.zoning = undefined
+            }
+	    normDec._version = decisionsVersion
+            await decisions.replaceOne({ _id: normalized[process.env.MONGO_ID] }, normDec)
+            normalizeCount++
+          }
+        }
+      } catch (e) {
+        console.error(e)
+        errorCount++
+      }
+
+      juricaOffset++
+    }
+    console.log(`Jurica - (${oldOffset}-${(oldOffset + batchSize)}) New: ${newCount}, Update: ${updateCount}, Normalize: ${normalizeCount}, Error: ${errorCount}.`)
+    if (newCount === 0 && updateCount === 0 && normalizeCount === 0) {
+      juricaEmptyRoundCount++
+    } else {
+      juricaEmptyRoundCount = 0
+    }
+    await client.close()
+  } else {
+    juricaOffset = 0
+    juricaEmptyRoundCount = 0
+  }
+
+  console.log('Teardown...')
+
+  // UPDATE histories
+  fs.writeFileSync(path.join(__dirname, 'offset.history'), `${jurinetOffset}`)
+  fs.writeFileSync(path.join(__dirname, 'emptyround.history'), `${jurinetEmptyRoundCount}`)
+  fs.writeFileSync(path.join(__dirname, 'offset_jurica.history'), `${juricaOffset}`)
+  fs.writeFileSync(path.join(__dirname, 'emptyround_jurica.history'), `${juricaEmptyRoundCount}`)
+  let hashesContent = ''
+  for (let id in jurinetHashes) {
+    hashesContent += `${id}:${jurinetHashes[id]}\n`
+  }
+  fs.writeFileSync(path.join(__dirname, 'hashes.history'), hashesContent.trim())
+  hashesContent = ''
+  for (let id in juricaHashes) {
+    hashesContent += `${id}:${juricaHashes[id]}\n`
+  }
+  fs.writeFileSync(path.join(__dirname, 'hashes_jurica.history'), hashesContent.trim())
+
+  console.log('Exit.')
+}
+
+async function getZones(id, source, text) {
+  const zoneData = JSON.stringify({
+    arret_id: id,
+    source: source,
+    text: text
+  })
+  const response = await needle('post', 'http://127.0.0.1:8090/zonage', zoneData, {
+    json: true
+  })
+  delete response.body.arret_id
+  return response.body
+}
+
+console.log('Run...')
+
+main()
---
+++ jurica-oracle.js
@@ -0,0 +1,167 @@
+require('dotenv').config()
+
+const iconv = require('iconv-lite')
+const oracledb = require('oracledb')
+
+iconv.skipDecodeWarning = true
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT
+
+// @TODO opt.parties
+
+class JuricaOracle {
+  constructor(opt) {
+    opt = opt || {}
+    this.verbose = opt.verbose || false
+    this.connected = false
+    this.connection = null
+  }
+
+  async connect() {
+    if (this.connected === false) {
+      this.connection = await oracledb.getConnection({
+        user: process.env.DB_USER_JURICA,
+        password: process.env.DB_PASS_JURICA,
+        connectString: process.env.DB_HOST_JURICA
+      })
+      this.connected = true
+      if (this.verbose === true) {
+        console.info(`Jurica - Connected to Oracle v${this.connection.oracleServerVersionString}.`)
+      }
+    } else {
+      throw new Error('Jurica - Already connected.')
+    }
+  }
+
+  async close() {
+    if (this.connected === true && this.connection !== null) {
+      await this.connection.close()
+      if (this.verbose === true) {
+        console.info('Jurica - Disconnected from Oracle.')
+      }
+    } else {
+      throw new Error('Jurica - Not connected.')
+    }
+  }
+
+  async describe() {
+    if (this.connected === true && this.connection !== null) {
+      // DESCRIBE-like query for an old version of Oracle:
+      const query = `SELECT *
+        FROM user_tab_columns
+        WHERE table_name = '${process.env.DB_TABLE_JURICA}'
+        ORDER BY column_id`
+      return await this.connection.execute(query)
+    } else {
+      throw new Error('Jurica - Not connected.')
+    }
+  }
+
+  async getBatch(opt) {
+    opt = opt || {}
+    opt.all = opt.all || false
+    opt.limit = opt.limit || 0
+    opt.offset = opt.offset || 0
+    opt.order = opt.order || 'ASC'
+    opt.titrage = opt.titrage || false
+
+    if (this.connected === true && this.connection !== null) {
+      let query = null
+
+      if (opt.all === true) {
+        // Get all documents:
+        query = `SELECT * 
+          FROM ${process.env.DB_TABLE_JURICA}
+          ORDER BY ${process.env.DB_ID_FIELD_JURICA} ${opt.order}`
+      } else {
+        // Only get the documents that are ready to be published:
+        query = `SELECT * 
+          FROM ${process.env.DB_TABLE_JURICA}
+          WHERE ${process.env.DB_ANO_TEXT_FIELD_JURICA} is not NULL
+          AND ${process.env.DB_VALID_FIELD_JURICA} is not NULL
+          AND ${process.env.DB_STATE_FIELD_JURICA} = :ok
+          ORDER BY ${process.env.DB_ID_FIELD_JURICA} ${opt.order}`
+      }
+
+      // LIMIT-like query for an old version of Oracle:
+      if (opt.limit || opt.offset) {
+        if (opt.offset > 0) {
+          opt.limit += opt.offset
+          opt.offset++
+        }
+        query = `SELECT * FROM (
+          SELECT a.*, ROWNUM rnum FROM (
+            ${query}
+          ) a WHERE rownum <= ${opt.limit}
+        ) WHERE rnum >= ${opt.offset}`
+      }
+
+      let result = null
+
+      if (opt.all === true) {
+        result = await this.connection.execute(query)
+      } else {
+        result = await this.connection.execute(query, [process.env.DB_STATE_OK_JURICA])
+      }
+
+      if (result && result.rows && result.rows.length > 0) {
+        let rows = []
+        for (let i = 0; i < result.rows.length; i++) {
+          let row = {}
+          for (let key in result.rows[i]) {
+            switch (key) {
+              case process.env.DB_ID_FIELD_JURICA:
+                row[process.env.MONGO_ID] = result.rows[i][key]
+                break;
+              case 'RNUM':
+                // Ignore RNUM key (added by offset/limit query)
+                break;
+              default:
+                try {
+                  if (typeof result.rows[i][key].getData === 'function') {
+                    row[key] = await result.rows[i][key].getData()
+                  } else {
+                    row[key] = result.rows[i][key]
+                  }
+                  row[key] = iconv.decode(row[key], process.env.ENCODING)
+                } catch (ignore) { }
+                break;
+            }
+          }
+          if (opt.titrage === true) {
+            // @TODO CAN'T FIND "TITRAGE" STUFF FOR JURICA...
+            // Inject "titrage" data (if any) into the result:
+            /*
+            const queryTitrage = `SELECT * 
+                FROM ${process.env.DB_TITRAGE_TABLE_JURICA}
+                WHERE ${process.env.DB_ID_FIELD_JURICA} = :id`
+            const resultTitrage = await this.connection.execute(queryTitrage, [result.rows[i][process.env.DB_ID_FIELD_JURICA]])
+            if (resultTitrage && resultTitrage.rows && resultTitrage.rows.length > 0) {
+              row[process.env.TITRAGE_FIELD] = []
+              for (let j = 0; j < resultTitrage.rows.length; j++) {
+                let titrageObj = {}
+                for (let key in resultTitrage.rows[j]) {
+                  titrageObj[key] = resultTitrage.rows[j][key]
+                  try {
+                    titrageObj[key] = iconv.decode(titrageObj[key], process.env.ENCODING)
+                  } catch (ignore) { }
+                }
+                row[process.env.TITRAGE_FIELD].push(titrageObj)
+              }
+            } else {
+              row[process.env.TITRAGE_FIELD] = null
+            }
+            */
+          }
+          rows.push(row)
+        }
+        return rows
+      } else {
+        return null
+      }
+    } else {
+      throw new Error('Jurica - Not connected.')
+    }
+  }
+}
+
+exports.JuricaOracle = JuricaOracle
