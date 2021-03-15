24e23862faec7c6271b8e303fb49d9763443573c
Nicolas Assouad <nicolas@assouad.com>
2021-03-15 11:03:38 +0100

    first test

---
+++ src/jurinet-utils.spec.js
@@ -0,0 +1,29 @@
+const JurinetUtils = require('./jurinet-utils').JurinetUtils;
+
+describe('JurinetUtils', () => {
+  describe('CleanXML', () => {
+    it('should do nothing with an already clean decision', async () => {
+      const xml = buildJurinetXml(['Some decision content']);
+
+      const cleanedXml = JurinetUtils.CleanXML(xml);
+
+      expect(cleanedXml).toEqual(xml);
+    });
+
+    it('should merge the different arret tag into one', async () => {
+      const textArrets = ['Some arret tag', 'Another arret tag'];
+      const xml = buildJurinetXml(textArrets);
+
+      const cleanedXml = JurinetUtils.CleanXML(xml);
+
+      expect(cleanedXml).toEqual(buildJurinetXml([textArrets.join(' ')]));
+    });
+  });
+});
+
+function buildJurinetXml(textArrets) {
+  const textArretsWithTags = textArrets.map((textArret) => `<TEXTE_ARRET>${textArret}</TEXTE_ARRET>`);
+  const xmlDocument = `<DOCUMENT>${textArretsWithTags.join('')}</DOCUMENT>`;
+
+  return xmlDocument;
+}
