f67b82de65368719db127a95a93699e028c8211f
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-05-04 12:51:51 +0200

    Does not work as expected...

--- src/index.js
+++ src/index.js
@@ -12,17 +12,14 @@ const bree = new Bree({
     {
       name: 'import',
       interval: 'every 5 minutes after 8:00am and before 12:00pm',
-      closeWorkerAfterMs: ms('30m'),
     },
     {
       name: 'reinject',
       interval: 'at 12:00 am',
-      closeWorkerAfterMs: ms('1h'),
     },
     {
       name: 'sync',
       interval: 'every 7 minutes after 9:00am and before 11:00pm',
-      closeWorkerAfterMs: ms('30m'),
     },
   ],
 });
