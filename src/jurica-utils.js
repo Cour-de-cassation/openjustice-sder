e2be70bb88e482ca9f0f4fd36fe78c4b2ed93b14
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-05-25 16:33:06 +0200

    Portalis ID are not ID...

--- src/jurica-utils.js
+++ src/jurica-utils.js
@@ -170,12 +170,37 @@ class JuricaUtils {
       throw new Error(`JuricaUtils.GetJurinetDuplicate: Jurica document ${id} has no Portalis ID.`);
     }
 
-    const jurinetDoc = await rawJurinet.findOne({ _portalis: juricaDoc._portalis });
+    const juricaDate = new Date(Date.parse(juricaDoc.JDEC_DATE));
+    const juricaDateTop = new Date(Date.parse(juricaDoc.JDEC_DATE));
+    const juricaDateBottom = new Date(Date.parse(juricaDoc.JDEC_DATE));
+    juricaDateTop.setDate(juricaDateTop.getDate() + 1);
+    juricaDateBottom.setDate(juricaDateBottom.getDate() - 1);
+
+    let found = null;
+    let jurinetDoc;
+    const cursor = await rawJurinet.find({ _portalis: juricaDoc._portalis }, { allowDiskUse: true });
+    while ((jurinetDoc = await cursor.next())) {
+      const jurinetDate = jurinetDoc.DT_DECISION;
+      if (
+        found === null &&
+        ((juricaDate.getFullYear() === jurinetDate.getFullYear() &&
+          juricaDate.getMonth() === jurinetDate.getMonth() &&
+          juricaDate.getDate() === jurinetDate.getDate()) ||
+          (juricaDateTop.getFullYear() === jurinetDate.getFullYear() &&
+            juricaDateTop.getMonth() === jurinetDate.getMonth() &&
+            juricaDateTop.getDate() === jurinetDate.getDate()) ||
+          (juricaDateBottom.getFullYear() === jurinetDate.getFullYear() &&
+            juricaDateBottom.getMonth() === jurinetDate.getMonth() &&
+            juricaDateBottom.getDate() === jurinetDate.getDate()))
+      ) {
+        found = jurinetDoc._id;
+      }
+    }
     await client.close();
-    if (jurinetDoc === null) {
+    if (found === null) {
       return null;
     }
-    return jurinetDoc._id;
+    return found;
   }
 }
 
