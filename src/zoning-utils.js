37d01dcec594f77b8cec36d355fc559226efa623
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-09-14 22:24:23 +0200

    About to implement new calls to the NLP API.

--- src/zoning-utils.js
+++ src/zoning-utils.js
@@ -2,14 +2,20 @@ const needle = require('needle');
 
 class ZoningUtils {
   static async getZones(id, source, text) {
-    const zoneData = JSON.stringify({
+    const zoneData = {
       arret_id: id,
       source: source,
       text: text,
-    });
-    const response = await needle('post', 'http://10.16.64.7:8090/zonage', zoneData, {
+    };
+    const response = await needle('post', `http://${process.env.ZONING_URI}/zonage`, zoneData, {
       json: true,
     });
+    if (!response || !response.body || !response.body.zones) {
+      console.warn('Zoning failed for the given document.', {
+        arret_id: id,
+        source: source,
+      });
+    }
     delete response.body.arret_id;
     return response.body;
   }
