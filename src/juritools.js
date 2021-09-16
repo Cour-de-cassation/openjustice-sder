cf7b0bceeacbf401d425420f3e98292a50427d22
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-09-16 10:53:30 +0200

    "zoning" is now part of "juritools".

---
+++ src/juritools.js
@@ -0,0 +1,51 @@
+const needle = require('needle');
+
+class Juritools {
+  static async GetZones(id, source, text) {
+    const zoneData = {
+      arret_id: id,
+      source: source,
+      text: text,
+    };
+    const response = await needle(
+      'post',
+      `http://${process.env.ZONING_URI}:${process.env.ZONING_PORT}/zonage`,
+      zoneData,
+      {
+        json: true,
+      },
+    );
+    if (!response || !response.body || !response.body.zones) {
+      console.warn('GetZones failed for the given data', zoneData);
+    }
+    delete response.body.arret_id;
+    return response.body;
+  }
+
+  static async GetMetaJurinet(data) {
+    const response = await needle(
+      'post',
+      `http://${process.env.META_URI}:${process.env.META_PORT}/meta_jurinet`,
+      data,
+      {
+        json: true,
+      },
+    );
+    if (!response || !response.body) {
+      console.warn('GetMetaJurinet failed for the given data', data);
+    }
+    return response.body;
+  }
+
+  static async GetMetaJurica(data) {
+    const response = await needle('post', `http://${process.env.META_URI}:${process.env.META_PORT}/meta_jurica`, data, {
+      json: true,
+    });
+    if (!response || !response.body) {
+      console.warn('GetMetaJurica failed for the given data', data);
+    }
+    return response.body;
+  }
+}
+
+exports.Juritools = Juritools;
