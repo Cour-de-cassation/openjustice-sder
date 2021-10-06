f5ff634101d49857b06038255f4a26fc3f40ab9f
rhanka <fabien.antoine@m4x.org>
2021-10-06 15:10:20 +0200

    update test

--- src/scripts/test_parties.js
+++ src/scripts/test_parties.js
@@ -1,6 +1,26 @@
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 
+const parser = require('fast-xml-parser');
+
+const parserOptions = {
+  attributeNamePrefix: '',
+  attrNodeName: 'attributes',
+  textNodeName: 'value',
+  ignoreAttributes: false,
+  ignoreNameSpace: true,
+  allowBooleanAttributes: false,
+  parseNodeValue: false,
+  parseAttributeValue: false,
+  trimValues: true,
+  cdataTagName: false,
+  parseTrueNumberOnly: false,
+  arrayMode: true,
+  trimValues: true,
+};
+
+const { MongoClient } = require('mongodb');
+
 async function main() {
   const client = new MongoClient(process.env.MONGO_URI, {
     useUnifiedTopology: true,
@@ -11,9 +31,18 @@ async function main() {
   const rawJurica = database.collection(process.env.MONGO_JURICA_COLLECTION);
   const decisions = database.collection(process.env.MONGO_DECISIONS_COLLECTION);
 
-  const rawDocument = rawJurica.findOne({ _id: 2437803 });
+  const rawDocument = await rawJurica.findOne({ _id: 2437803 });
+  const xml = `<document>${rawDocument.JDEC_COLL_PARTIES}</document>`;
 
-  console.log(rawDocument.JDEC_COLL_PARTIES);
+  console.log(xml);
+
+  const valid = parser.validate(xml);
+  if (valid === true) {
+  	const json = parser.parse(xml, parserOptions);
+	console.log(JSON.stringify(json.document[0].partie, null, 2));
+  } else {
+	console.error('invalid xml', valid);
+  }
 
   await client.close();
   return true;
