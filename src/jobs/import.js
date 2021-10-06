7711dcd94e19190e3db20b39af6c2ef1d4e80256
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-10-06 21:43:48 +0200

    Import decatt (wip)

--- src/jobs/import.js
+++ src/jobs/import.js
@@ -32,14 +32,19 @@ function kill(code) {
 async function main() {
   console.log('OpenJustice - Start "import" job:', new Date().toLocaleString());
   try {
-    await importJurinet();
+    // await importJurinet();
   } catch (e) {
     console.error('Jurinet import error', e);
   }
   try {
-    await importJurica();
+    // await importJurica();
   } catch (e) {
     console.error('Jurica import error', e);
+  }
+  try {
+    await importDecatt();
+  } catch (e) {
+    console.error('Decatt import error', e);
   }
   console.log('OpenJustice - End "import" job:', new Date().toLocaleString());
   setTimeout(end, ms('1s'));
@@ -256,6 +261,51 @@ async function importJurica() {
   );
   await client.close();
   await juricaSource.close();
+  return true;
+}
+
+async function importDecatt() {
+  const client = new MongoClient(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const database = client.db(process.env.MONGO_DBNAME);
+  const rawJurinet = database.collection(process.env.MONGO_JURINET_COLLECTION);
+  const rawJurica = database.collection(process.env.MONGO_JURICA_COLLECTION);
+  const decisions = database.collection(process.env.MONGO_DECISIONS_COLLECTION);
+
+  // 1. Get all _decatt from rawJurinet (no other choice, really)...
+  let allDecatt = [];
+  let rawJurinetDocument;
+  const rawJurinetCursor = await rawJurinet
+    .find(
+      { TYPE_ARRET: 'CC', _decatt: { $ne: null } },
+      {
+        allowDiskUse: true,
+        fields: {
+          _id: 1,
+          _decatt: 1,
+        },
+      },
+    )
+    .sort({ _id: -1 });
+  while ((rawJurinetDocument = await rawJurinetCursor.next())) {
+    if (
+      rawJurinetDocument._decatt &&
+      Array.isArray(rawJurinetDocument._decatt) &&
+      rawJurinetDocument._decatt.length > 0
+    ) {
+      for (let i = 0; i < rawJurinetDocument._decatt.length; i++) {
+        if (allDecatt.indexOf(rawJurinetDocument._decatt[i]) === -1) {
+          allDecatt.push(rawJurinetDocument._decatt[i]);
+        }
+      }
+    }
+  }
+
+  console.log(allDecatt.length);
+
+  await client.close();
   return true;
 }
 
