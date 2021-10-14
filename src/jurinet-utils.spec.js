d762b44ada5e1e36610e25be7eff39beb0f0d6d6
BenoitSerrano <benoit.serrano10@gmail.com>
2021-10-14 12:44:47 +0200

    handle categoriesToOmit via occultationBlock

--- src/jurinet-utils.spec.js
+++ src/jurinet-utils.spec.js
@@ -1,4 +1,5 @@
 const JurinetUtils = require('./jurinet-utils').JurinetUtils;
+const ConvertOccultationBlockInCategoriesToOmit = require('./jurinet-utils').ConvertOccultationBlockInCategoriesToOmit;
 
 describe('JurinetUtils', () => {
   describe('CleanXML', () => {
@@ -19,7 +20,71 @@ describe('JurinetUtils', () => {
       expect(cleanedXml).toEqual(buildJurinetXml([textArrets.join(' ')]));
     });
   });
+
 });
+
+describe('ConvertOccultationBlockInCategoriesToOmit', () => {
+  it('should return categories for block 1', () => {
+    const categoriesToOmit = ConvertOccultationBlockInCategoriesToOmit(1);
+
+    expect(categoriesToOmit.sort()).toEqual(
+      ['professionnelMagistratGreffier'].sort(),
+    );
+  });
+
+  it('should return categories for block 2', () => {
+    const categoriesToOmit = ConvertOccultationBlockInCategoriesToOmit(2);
+
+    expect(categoriesToOmit.sort()).toEqual(
+      [
+        'professionnelMagistratGreffier',
+        'dateNaissance',
+        'dateMariage',
+        'dateDeces',
+      ].sort(),
+    );
+  });
+
+  it('should return categories for block 3', () => {
+    const categoriesToOmit = ConvertOccultationBlockInCategoriesToOmit(3);
+
+    expect(categoriesToOmit.sort()).toEqual(
+      [
+        'professionnelMagistratGreffier',
+        'personneMorale',
+        'numeroSiretSiren',
+      ].sort(),
+    );
+  });
+
+  it('should return categories for block 4', () => {
+    const categoriesToOmit = ConvertOccultationBlockInCategoriesToOmit(4);
+
+    expect(categoriesToOmit.sort()).toEqual(
+      [
+        'professionnelMagistratGreffier',
+        'dateNaissance',
+        'dateMariage',
+        'dateDeces',
+        'personneMorale',
+        'numeroSiretSiren',
+      ].sort(),
+    );
+  });
+
+  it('should return categories for block null', () => {
+    const categoriesToOmit = ConvertOccultationBlockInCategoriesToOmit(null);
+
+    expect(categoriesToOmit.sort()).toEqual(
+      [
+        'professionnelMagistratGreffier',
+        'personneMorale',
+        'numeroSiretSiren',
+      ].sort(),
+    );
+  });
+});
+
 
 function buildJurinetXml(textArrets) {
   const textArretsWithTags = textArrets.map((textArret) => `<TEXTE_ARRET>${textArret}</TEXTE_ARRET>`);
