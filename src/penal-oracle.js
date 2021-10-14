9046cf6eddc82bbf2e166ab3fb1d73c966c1c668
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-10-14 23:15:15 +0200

    Add connector to penal db

---
+++ src/penal-oracle.js
@@ -0,0 +1,84 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+
+const iconv = require('iconv-lite');
+const oracledb = require('oracledb');
+
+iconv.skipDecodeWarning = true;
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+class PenalOracle {
+  constructor() {
+    this.connected = false;
+    this.connection = null;
+  }
+
+  async connect() {
+    if (this.connected === false) {
+      this.connection = await oracledb.getConnection({
+        user: process.env.PENAL_DB_USER,
+        password: process.env.PENAL_DB_PASS,
+        connectString: process.env.PENAL_DB_HOST,
+      });
+      this.connected = true;
+    } else {
+      throw new Error('PenalOracle.connect: already connected.');
+    }
+  }
+
+  async close() {
+    if (this.connected === true && this.connection !== null) {
+      await this.connection.close();
+    } else {
+      throw new Error('PenalOracle.close: not connected.');
+    }
+  }
+
+  async describe(table) {
+    if (this.connected === true && this.connection !== null) {
+      // DESCRIBE-like query for an old version of Oracle:
+      const query = `SELECT *
+        FROM user_tab_columns
+        WHERE table_name = :table
+        ORDER BY column_id`;
+      return await this.connection.execute(query, [table]);
+    } else {
+      throw new Error('PenalOracle.describe: not connected.');
+    }
+  }
+
+  async buildRawData(row) {
+    if (this.connected === true && this.connection !== null) {
+      let data = {};
+      for (let key in row) {
+        switch (key) {
+          case 'rnum':
+            // Ignore rnum key (added by offset/limit queries)
+            break;
+          case 'RNUM':
+            // Ignore RNUM key (added by offset/limit queries)
+            break;
+          default:
+            if (row[key] && typeof row[key].getData === 'function') {
+              try {
+                data[key] = await row[key].getData();
+              } catch (e) {
+                data[key] = null;
+              }
+            } else {
+              data[key] = row[key];
+            }
+            if (Buffer.isBuffer(data[key])) {
+              data[key] = iconv.decode(data[key], 'CP1252');
+            }
+            break;
+        }
+      }
+      return data;
+    } else {
+      throw new Error('PenalOracle.buildRawData: not connected.');
+    }
+  }
+}
+
+exports.PenalOracle = PenalOracle;
