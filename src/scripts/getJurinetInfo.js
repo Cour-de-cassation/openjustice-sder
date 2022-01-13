113b7061a880af3871caad47ec2a0c53df49277b
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2022-01-13 14:53:01 +0100

    Same for Jurinet

--- src/scripts/getJurinetInfo.js
+++ src/scripts/getJurinetInfo.js
@@ -2,6 +2,8 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 
 const { JurinetOracle } = require('../jurinet-oracle');
+const { JurinetUtils } = require('../jurinet-utils');
+const { Juritools } = require('../juritools');
 
 async function main() {
   let id;
@@ -32,13 +34,29 @@ async function getJurinetInfo(id) {
   let resultRow;
 
   while ((resultRow = await rs.getRow())) {
-    rows.push(await jurinetSource.buildRawData(resultRow, false));
+    rows.push(await jurinetSource.buildRawData(resultRow, true));
   }
 
   await rs.close();
   await jurinetSource.close();
 
   console.log(JSON.stringify(rows[0], null, 2));
+
+  const normalizedDecision = await JurinetUtils.Normalize(rows[0]);
+
+  if (normalizedDecision.originalText) {
+    try {
+      const zoning = await Juritools.GetZones(
+        normalizedDecision.sourceId,
+        normalizedDecision.sourceName,
+        normalizedDecision.originalText,
+        'https://nlp-jurizonage-api.judilibre-prive.local',
+      );
+      console.log(JSON.stringify(zoning, null, 2));
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   return true;
 }
