3bb3774d019a25049244346a4e935ca28339085a
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2022-01-17 21:37:24 +0100

    Weird error messages

--- src/judifiltre.js
+++ src/judifiltre.js
@@ -3,7 +3,7 @@ const needle = require('needle');
 class Judifiltre {
   static async SendBatch(batch, host) {
     if (host === undefined) {
-      host = `${process.env.JUDIFILTRE_PROTOCOL}://${process.env.JUDIFILTRE_URI}:${process.env.JUDIFILTRE_PORT}`;
+      host = `${process.env.JUDIFILTRE_PROTOCOL}://${process.env.JUDIFILTRE_URI}`;
     }
     console.log(`${host}/judifiltre/api/publicityInfos`);
     const response = await needle('post', `${host}/judifiltre/api/publicityInfos`, batch, {
