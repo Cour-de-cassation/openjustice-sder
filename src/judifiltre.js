413ac56a0d14c093f373247912ac8e53016c606d
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2022-01-17 23:05:40 +0100

    nope

--- src/judifiltre.js
+++ src/judifiltre.js
@@ -5,8 +5,8 @@ class Judifiltre {
     if (host === undefined) {
       host = `${process.env.JUDIFILTRE_PROTOCOL}://${process.env.JUDIFILTRE_URI}`;
     }
-    console.log(`${host}/api/publicityInfos`);
-    const response = await needle('post', `${host}/api/publicityInfos`, batch, {
+    console.log(`${host}/judifiltre/api/publicityInfos`);
+    const response = await needle('post', `${host}/judifiltre/api/publicityInfos`, batch, {
       json: true,
       rejectUnauthorized: false,
     });
