d01baf80dcf2426ca0a009183e44062ead848ef4
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2022-01-19 14:23:14 +0100

    Testing

--- src/scripts/retry_judifiltre.js
+++ src/scripts/retry_judifiltre.js
@@ -2,10 +2,18 @@ const { fail } = require('assert');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 
+const { MongoClient } = require('mongodb');
 const { Judifiltre } = require('../judifiltre');
 const { JudilibreIndex } = require('../judilibre-index');
 
 async function main() {
+  const client = new MongoClient(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const database = client.db(process.env.MONGO_DBNAME);
+  const rawJurica = database.collection(process.env.MONGO_JURICA_COLLECTION);
+
   const failedDocs = await JudilibreIndex.find('mainIndex', {
     $or: [
       {
@@ -20,7 +28,47 @@ async function main() {
   console.log(failedDocs.length);
 
   for (let i = 0; i < failedDocs.length; i++) {
-    console.log(failedDocs[i]._id);
+    console.log(`retry ${failedDocs[i]._id} (${i + 1}/${failedDocs.length})...`);
+
+    try {
+      let row = await rawJurica.findOne({ _id: parseInt(failedDocs[i]._id.split(':')[1], 10) });
+
+      if (row) {
+        console.log({
+          decisionDate: row.JDEC_DATE,
+          sourceDb: 'jurica',
+          sourceId: row._id,
+          jurisdiction: row.JDEC_CODE_JURIDICTION,
+          clerkRequest:
+            row.JDEC_IND_DEC_PUB === null
+              ? 'unspecified'
+              : parseInt(`${row.JDEC_IND_DEC_PUB}`, 10) === 1
+              ? 'public'
+              : 'notPublic',
+          fieldCode: row.JDEC_CODNAC + (row.JDEC_CODNACPART ? '-' + row.JDEC_CODNACPART : ''),
+        });
+      }
+
+      /*
+      const judifiltreResult = await Judifiltre.SendBatch([
+        {
+          decisionDate: row.JDEC_DATE,
+          sourceDb: 'jurica',
+          sourceId: row._id,
+          jurisdiction: row.JDEC_CODE_JURIDICTION,
+          clerkRequest:
+            row.JDEC_IND_DEC_PUB === null
+              ? 'unspecified'
+              : parseInt(`${row.JDEC_IND_DEC_PUB}`, 10) === 1
+              ? 'public'
+              : 'notPublic',
+          fieldCode: row.JDEC_CODNAC + (row.JDEC_CODNACPART ? '-' + row.JDEC_CODNACPART : ''),
+        },
+      ]);
+      */
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   /*
@@ -37,6 +85,8 @@ async function main() {
 
   console.log(result);
   */
+
+  await client.close();
 
   return true;
 }
