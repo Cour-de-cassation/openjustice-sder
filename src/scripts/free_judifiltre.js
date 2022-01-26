3ce3cacaa851476dfa04e3544c538d4a23b95f91
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2022-01-26 16:44:15 +0100

    last check

--- src/scripts/free_judifiltre.js
+++ src/scripts/free_judifiltre.js
@@ -4,6 +4,8 @@ require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 const { MongoClient } = require('mongodb');
 const { Judifiltre } = require('../judifiltre');
 const { JudilibreIndex } = require('../judilibre-index');
+const { JuricaUtils } = require('../jurica-utils');
+const decisionsVersion = parseFloat(process.env.MONGO_DECISIONS_VERSION);
 
 async function main() {
   const client = new MongoClient(process.env.MONGO_URI, {
@@ -23,9 +25,24 @@ async function main() {
 
     try {
       let row = await rawJurica.findOne({ _id: parseInt(queueDocs[i]._id.split(':')[1], 10) });
-
       if (row) {
-        console.log('ok');
+        let normalized = await decisions.findOne({ sourceId: row._id, sourceName: 'jurica' });
+        if (normalized === null) {
+          /*
+          let normDec = await JuricaUtils.Normalize(row);
+          normDec.originalText = JuricaUtils.removeMultipleSpace(normDec.originalText);
+          normDec.originalText = JuricaUtils.replaceErroneousChars(normDec.originalText);
+          normDec.pseudoText = JuricaUtils.removeMultipleSpace(normDec.pseudoText);
+          normDec.pseudoText = JuricaUtils.replaceErroneousChars(normDec.pseudoText);
+          normDec._version = decisionsVersion;
+          const insertResult = await decisions.insertOne(normDec, { bypassDocumentValidation: true });
+          normDec._id = insertResult.insertedId;
+          await JudilibreIndex.indexDecisionDocument(normDec, null, 'import in decisions');
+          */
+          console.log('ok');
+        } else {
+          console.log('skip');
+        }
       } else {
         console.log('NOK');
       }
