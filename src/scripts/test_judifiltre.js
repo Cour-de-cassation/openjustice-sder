8cced664ef436e90ae59606ac5938ab03790b7fb
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2022-02-17 18:50:24 +0100

    What's wrong with Jurica?

--- src/scripts/test_judifiltre.js
+++ src/scripts/test_judifiltre.js
@@ -2,8 +2,31 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 
 const { Judifiltre } = require('../judifiltre');
+const { MongoClient } = require('mongodb');
 
 async function main() {
+  const client = new MongoClient(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const database = client.db(process.env.MONGO_DBNAME);
+  const rawJurica = database.collection(process.env.MONGO_JURICA_COLLECTION);
+
+  let doc;
+  let count = 0;
+  const cursor = await rawJurica.find({ JDEC_IND_DEC_PUB: { $ne: null } }, { allowDiskUse: true });
+  while ((doc = await cursor.next())) {
+    console.log({
+      id: doc._id,
+      pub: doc.JDEC_IND_DEC_PUB,
+    });
+    count++;
+  }
+
+  console.log(count);
+
+  await client.close();
+
   /*
   const result = await Judifiltre.SendBatch([
     {
@@ -17,6 +40,7 @@ async function main() {
   ]);
   */
 
+  /*
   const batch = await Judifiltre.GetBatch();
 
   console.log(batch);
@@ -24,6 +48,7 @@ async function main() {
   const queue = await Judifiltre.GetQueue();
 
   console.log(queue);
+  */
 
   return true;
 }
