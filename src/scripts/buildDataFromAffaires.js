79852c0cbcc4794e1a38d15899a85be5b4dd55be
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2022-04-06 13:27:24 +0200

    Boom

--- src/scripts/buildDataFromAffaires.js
+++ src/scripts/buildDataFromAffaires.js
@@ -1,7 +1,10 @@
+const { raw } = require('express');
+const fs = require('fs');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 
 async function main() {
+  const { JuricaUtils } = require('../jurica-utils');
   const { MongoClient, ObjectId } = require('mongodb');
 
   const jIndexConnection = new MongoClient(process.env.INDEX_DB_URI, {
@@ -16,41 +19,82 @@ async function main() {
   const rawJurica = SDERClient.collection('rawJurica');
   const decisions = SDERClient.collection('decisions');
 
+  const data = [];
   const jurinetIds = [];
   const juricaIds = [];
   let doc;
   let cursor = await jIndexAffaires.find({ ids: { $ne: [] } });
   while ((doc = await cursor.next())) {
+    let hasStuff = false;
+    const jurinetDocs = [];
+    const juricaDocs = [];
     if (Array.isArray(doc.ids) && doc.ids.length > 0) {
       for (let i = 0; i < doc.ids.length; i++) {
         if (doc.ids[i].indexOf('jurinet') !== -1 && jurinetIds.indexOf(doc.ids[i]) === -1) {
+          jurinetIds.push(doc.ids[i]);
           const jurinetDoc = await decisions.findOne({
             sourceName: 'jurinet',
             sourceId: parseInt(doc.ids[i].split(':')[1]),
           });
           if (jurinetDoc !== null && (jurinetDoc.labelStatus === 'done' || jurinetDoc.labelStatus === 'exported')) {
+            const rawJurinetDoc = await rawJurinet.findOne({
+              _id: parseInt(doc.ids[i].split(':')[1]),
+            });
+            jurinetDocs.push({
+              raw: rawJurinetDoc,
+              decision: jurinetDoc,
+            });
+            hasStuff = true;
             console.log(`add ${doc.ids[i]}`);
-            jurinetIds.push(doc.ids[i]);
           }
         } else if (doc.ids[i].indexOf('jurica') !== -1 && juricaIds.indexOf(doc.ids[i]) === -1) {
+          juricaIds.push(doc.ids[i]);
           const juricaDoc = await decisions.findOne({
             sourceName: 'jurica',
             sourceId: parseInt(doc.ids[i].split(':')[1]),
           });
           if (juricaDoc !== null && (juricaDoc.labelStatus === 'done' || juricaDoc.labelStatus === 'exported')) {
-            console.log(`add ${doc.ids[i]}`);
-            juricaIds.push(doc.ids[i]);
+            const rawJuricaDoc = await rawJurica.findOne({
+              _id: parseInt(doc.ids[i].split(':')[1]),
+            });
+            try {
+              if (
+                JuricaUtils.IsPublic(
+                  rawJuricaDoc.JDEC_CODNAC,
+                  rawJuricaDoc.JDEC_CODNACPART,
+                  rawJuricaDoc.JDEC_IND_DEC_PUB,
+                ) ||
+                JuricaUtils.IsPartiallyPublic(
+                  rawJuricaDoc.JDEC_CODNAC,
+                  rawJuricaDoc.JDEC_CODNACPART,
+                  rawJuricaDoc.JDEC_IND_DEC_PUB,
+                )
+              ) {
+                juricaDoc.push({
+                  raw: rawJuricaDoc,
+                  decision: juricaDoc,
+                });
+                hasStuff = true;
+                console.log(`add ${doc.ids[i]}`);
+              }
+            } catch (ignore) {}
           }
         }
       }
+    }
+    if (hasStuff === true) {
+      data.push({
+        affaire: doc,
+        jurinet: jurinetDocs,
+        jurica: juricaDocs,
+      });
     }
   }
 
   await cursor.close();
   await jIndexConnection.close();
 
-  console.log('jurinet', jurinetIds.length);
-  console.log('jurica', juricaIds.length);
+  fs.writeFileSync('dataFromAffaires.json', JSON.stringify(data));
 }
 
 main();
