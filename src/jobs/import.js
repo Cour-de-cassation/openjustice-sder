6aeea7ebc5b81751dae1ec680dba9180208a2643
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-04-09 16:57:54 +0200

    update import (#202)

--- src/jobs/import.js
+++ src/jobs/import.js
@@ -1355,7 +1355,6 @@ async function syncJurica() {
           }
           row.JDEC_HTML_SOURCE = parts.join('\n\n[...]\n\n');
         }
-        await JuricaUtils.IndexAffaire(row, jIndexMain, jIndexAffaires, jurinetSource.connection);
         const ShouldBeSentToJudifiltre = JuricaUtils.ShouldBeSentToJudifiltre(
           row.JDEC_CODNAC,
           row.JDEC_CODNACPART,
