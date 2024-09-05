24e188a836625e73f791ea3e8c0cdcfa4b62ed6b
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-09-05 12:59:33 +0200

    We may have a problem...

--- src/scripts/extractSafeCCFromOracle.js
+++ src/scripts/extractSafeCCFromOracle.js
@@ -309,6 +309,17 @@ async function parseOracleData(data, tableName, schema) {
           }
           schema[tableName][key].CP1252 = true;
           parsed[key] = iconv.decode(parsed[key], 'CP1252');
+        } else {
+          const test = iconv.decode(Buffer.from(`${parsed[key]}`), 'CP1252');
+          if (`${parsed[key]}` !== test) {
+            if (schema[tableName] === undefined) {
+              schema[tableName] = {};
+            }
+            if (schema[tableName][key] === undefined) {
+              schema[tableName][key] = {};
+            }
+            schema[tableName][key].PROBLEM = true;
+          }
         }
         break;
     }
