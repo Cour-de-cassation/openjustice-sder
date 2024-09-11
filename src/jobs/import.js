acbdc3119f58ac4234de30d7d19709de278720b6
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-09-11 10:21:14 +0200

    Bonjour

--- src/jobs/import.js
+++ src/jobs/import.js
@@ -37,7 +37,7 @@ function kill(code) {
 
 async function main() {
   console.log(
-    `OpenJustice - Start "import" job v20240620_2 on env ${process.env.NODE_ENV}:`,
+    `OpenJustice - Start "import" job v20240911_1 on env ${process.env.NODE_ENV}:`,
     new Date().toLocaleString(),
   );
   try {
