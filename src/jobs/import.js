bb1e5cf4745c64cc79258dd94c530ea3808651a2
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-11-07 12:22:53 +0100

    Update import.js

--- src/jobs/import.js
+++ src/jobs/import.js
@@ -1115,7 +1115,9 @@ async function syncJurinet() {
                 await JudilibreIndex.updateJurinetDocument(
                   row,
                   null,
-                  `update in rawJurinet (sync2) - Skip decision (too old: ${row.DT_DECISION.toISOString()}) - changelog: ${JSON.stringify(changelog)}`,
+                  `update in rawJurinet (sync2) - Skip decision (too old: ${row.DT_DECISION.toISOString()}) - changelog: ${JSON.stringify(
+                    changelog,
+                  )}`,
                 );
               } else if (tooEarly === true && hasException === false) {
                 updateCount++;
@@ -1125,7 +1127,9 @@ async function syncJurinet() {
                 await JudilibreIndex.updateJurinetDocument(
                   row,
                   null,
-                  `update in rawJurinet (sync2) - Skip decision (too early: ${Math.round(dateDiff2.days)} days) - changelog: ${JSON.stringify(changelog)}`,
+                  `update in rawJurinet (sync2) - Skip decision (too early: ${Math.round(
+                    dateDiff2.days,
+                  )} days) - changelog: ${JSON.stringify(changelog)}`,
                 );
               } else {
                 row._indexed = null;
@@ -1308,7 +1312,11 @@ async function syncJurinet() {
     console.log(`Done Syncing Jurinet - Empty round.`);
   }
 
-  fs.writeFileSync(path.join(__dirname, 'data', 'jurinet.lastDate'), jurinetLastDate.toISO());
+  try {
+    fs.writeFileSync(path.join(__dirname, 'data', 'jurinet.lastDate'), jurinetLastDate.toISO());
+  } catch (e) {
+    console.error(e);
+  }
 
   await jurinetSource.close();
 
@@ -1851,8 +1859,12 @@ async function syncJurica() {
 
   await juricaSource.close();
 
-  fs.writeFileSync(path.join(__dirname, 'data', 'jurica.lastDate'), juricaLastDate.toISO());
-
+  try {
+    fs.writeFileSync(path.join(__dirname, 'data', 'jurica.lastDate'), juricaLastDate.toISO());
+  } catch (e) {
+    console.error(e);
+  }
+  
   return true;
 }
 
