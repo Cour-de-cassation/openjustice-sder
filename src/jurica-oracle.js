146588f2c587a2e4f05f927309ac864d9f16fa64
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-11-07 12:16:42 +0100

    Get CA decisions from the end

--- src/jurica-oracle.js
+++ src/jurica-oracle.js
@@ -181,7 +181,7 @@ class JuricaOracle {
         AND ${process.env.DB_TABLE_JURICA}.${process.env.DB_ANO_TEXT_FIELD_JURICA} IS NULL
         AND ${process.env.DB_TABLE_JURICA}.${process.env.DB_STATE_FIELD_JURICA} = 0
         AND ${process.env.DB_TABLE_JURICA}.JDEC_DATE_CREATION >= ${strAgo}
-        ORDER BY ${process.env.DB_TABLE_JURICA}.${process.env.DB_ID_FIELD_JURICA} ASC`;
+        ORDER BY ${process.env.DB_TABLE_JURICA}.${process.env.DB_ID_FIELD_JURICA} DESC`;
 
       query = `SELECT * FROM (
         SELECT a.*, ROWNUM rnum FROM (
