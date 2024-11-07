5e055ef6be7f9d4f13e22a67ff43af2d422526a3
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-11-07 12:55:55 +0100

    Speedup (#285)

--- src/jurica-oracle.js
+++ src/jurica-oracle.js
@@ -172,10 +172,8 @@ class JuricaOracle {
       ago.setMonth(ago.getMonth() - monthAgo);
       ago.setHours(0, 0, 0, 0);
       let strAgo = ago.getFullYear();
-      strAgo = `${strAgo}` + (ago.getMonth() + 1 < 10 ? '0' + (ago.getMonth() + 1) : `${ago.getMonth() + 1}`);
-      strAgo = `${strAgo}` + ago.getDate() < 10 ? '0' + ago.getDate() : `${ago.getDate()}`;
-
-      console.log(`JuricaOracle:getNew(${monthAgo}): ${strAgo}.`);
+      strAgo = `${strAgo}${ago.getMonth() + 1 < 10 ? '0' + (ago.getMonth() + 1) : ago.getMonth() + 1}`;
+      strAgo = `${strAgo}${ago.getDate() < 10 ? '0' + ago.getDate() : ago.getDate()}`;
 
       console.log(`JuricaOracle:getNew(${monthAgo}): ${strAgo}.`);
 
@@ -304,9 +302,9 @@ class JuricaOracle {
       ago.setMonth(ago.getMonth() - NMonth);
       ago.setHours(0, 0, 0, 0);
       let strAgo = ago.getFullYear();
-      strAgo = `${strAgo}` + (ago.getMonth() + 1 < 10 ? '0' + (ago.getMonth() + 1) : `${ago.getMonth() + 1}`);
-      strAgo = `${strAgo}` + ago.getDate() < 10 ? '0' + ago.getDate() : `${ago.getDate()}`;
-
+      strAgo = `${strAgo}${ago.getMonth() + 1 < 10 ? '0' + (ago.getMonth() + 1) : ago.getMonth() + 1}`;
+      strAgo = `${strAgo}${ago.getDate() < 10 ? '0' + ago.getDate() : ago.getDate()}`;
+      
       const query = `SELECT *
         FROM ${process.env.DB_TABLE_JURICA}
         WHERE ${process.env.DB_TABLE_JURICA}.JDEC_HTML_SOURCE IS NOT NULL
