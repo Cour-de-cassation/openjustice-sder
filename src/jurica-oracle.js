3a8ce4ea470de134fcf4e9435b85e0b5be66a934
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2026-03-18 15:34:46 +0100

    But keep error locking

--- src/jurica-oracle.js
+++ src/jurica-oracle.js
@@ -252,6 +252,7 @@ class JuricaOracle {
       const query = `SELECT *
         FROM ${process.env.DB_TABLE_JURICA}
         WHERE ${process.env.DB_TABLE_JURICA}.JDEC_HTML_SOURCE IS NOT NULL
+        AND ${process.env.DB_TABLE_JURICA}.${process.env.DB_STATE_FIELD_JURICA} != 4
         AND ${process.env.DB_TABLE_JURICA}.JDEC_DATE_MAJ > '${strDate}'
         ORDER BY ${process.env.DB_TABLE_JURICA}.${process.env.DB_ID_FIELD_JURICA} ASC`;
 
